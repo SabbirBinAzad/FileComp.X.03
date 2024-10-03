ed15c978592757f98224f5bd82361b810d18deef
SABBIR BIN AZAD <39020183+SabbirBinAzad@users.noreply.github.com>
2024-10-03 10:30:34 -0400

    Create Decompressor.h

---
+++ Decompressor.h
@@ -0,0 +1,11 @@
+#ifndef DECOMPRESSOR_H
+#define DECOMPRESSOR_H
+
+#include <string>
+
+class Decompressor {
+public:
+    void decompress(const std::string& inputFile, the std::string& outputFile);
+};
+
+#endif // DECOMPRESSOR_H
